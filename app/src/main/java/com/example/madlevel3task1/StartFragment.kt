75b24fc8e80cf0a31487c8ffa245812c3c33202f
Skander <skander@bibani.nl>
2020-10-18 19:24:22 +0200

    Fix start layout

--- app/src/main/java/com/example/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -22,6 +24,11 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnStartRating.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_StartFragment_to_RatingFragment
+            )
+        }
 
     }
 }
